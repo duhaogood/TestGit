ccaa2cd275c34050be7a25abc73b9a256fb76a5d
mac_hao <mac_hao@mac-haodeMac-mini.local>
2017-05-22 18:22:32 +0800

    123321

---
+++ testGit/testGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGit
+//
+//  Created by mac_hao on 2017/5/22.
+//  Copyright © 2017年 杜浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGit/testGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGit
+//
+//  Created by mac_hao on 2017/5/22.
+//  Copyright © 2017年 杜浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
